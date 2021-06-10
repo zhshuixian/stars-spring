5326df1db5e58acfcb231f6a2810d29010257539
小先 <zhshuixian@hotmail.com>
2021-06-10 17:17:05 +0800

    Spring-005:Resource 模块完成，现在头晕先提交

---
+++ src/main/java/org/stars/spring/core/io/DefaultResourceLoader.java
@@ -0,0 +1,27 @@
+package org.stars.spring.core.io;
+
+import cn.hutool.core.lang.Assert;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * 资源加载器的实现
+ * @author : xian
+ */
+public class DefaultResourceLoader implements ResourceLoader {
+    @Override
+    public Resource getResource(String location) {
+        Assert.notNull(location, "Location must be not null");
+        if(location.startsWith(CLASSPATH_URL_PREFIX)) {
+            return new ClassPathResource(location.substring(CLASSPATH_URL_PREFIX.length()));
+        } else {
+            try {
+                URL url = new URL(location);
+                return new UrlResource(url);
+            } catch (MalformedURLException e) {
+                return new FileSystemResource(location);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/stars/spring/core/io/ResourceLoader.java
@@ -0,0 +1,16 @@
+package org.stars.spring.core.io;
+
+/**
+ * @author : xian
+ */
+public interface ResourceLoader {
+
+    String CLASSPATH_URL_PREFIX = "classpath:";
+
+    /**
+     * 获取 Resource
+     * @param location 位置
+     * @return  Resource
+     */
+    Resource getResource(String location);
+}
